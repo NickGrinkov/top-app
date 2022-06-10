1f59ba77e6b3f3fe793939c661a545d33bcbbce6
NickGrinkov <nikitagr1801@gmail.com>
2022-06-10 12:23:47 +0300

    set buttons in app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+
+
+module.exports = {
+  nextConfig,
+  webpack(config) {
+  config.module.rules.push({
+    test: /\.svg$/,
+    issuer: /\.(js|ts)x?$/,
+    use: ["@svgr/webpack"],
+  });
+
+  return config;
+  }
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from "next/head";
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MyTop - лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
